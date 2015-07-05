32a2931424ac284dcecb26207b38d123c918a7de
Callum Rogers <callumrogers@hotmail.co.uk>
2015-07-05 18:32:51 +0100

    Bump intern, remove deprecation warnings from unit test

--- intern-config/intern-unit.js
+++ intern-config/intern-unit.js
@@ -1,16 +1,9 @@
 define({
-
-  loader: {
-    // Packages that should be registered with the loader in each testing environment
-    packages: [
-    ]
-  },
-
   suites: [
     'build/tests/unit/blah'
   ],
 
-  reporters: [ 'pretty' ],
+  reporters: [ 'Pretty' ],
 
   excludeInstrumentation: /./
 });
