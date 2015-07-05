59cf85b1c562b06245c3e825febc049f8f463a7d
Callum Rogers <callumrogers@hotmail.co.uk>
2015-07-05 15:27:05 +0100

    Another ETE test

--- src/tests/functional/test.ts
+++ src/tests/functional/test.ts
@@ -1,5 +1,6 @@
 /// <reference path="../../typings/intern/intern/intern.d.ts" />
 
+import expect = require('intern/chai!expect')
 import registerSuite = require('intern!object')
 import Command = require('leadfoot/Command')
 
@@ -8,12 +9,26 @@ var toUrl = (<any>require).toUrl;
 registerSuite({
     name: 'Gates should',
     'create a new 1-source after pressing the create button'() {
-        return (<Command>this['remote'])
+        return (<Command<void>>this['remote'])
             .get(toUrl('../../index.html'))
             .setFindTimeout(1000)
             .findById('create-one-source')
                 .click()
                 .end()
             .findByClassName('one-source');
+    },
+    'show the value of a new 1-source to be 1'() {
+        return (<Command<void>>this['remote'])
+            .get(toUrl('../../index.html'))
+            .setFindTimeout(1000)
+            .findById('create-one-source')
+                .click()
+                .end()
+            .findByClassName('one-source')
+                .click()
+                .end()
+            .findById('value')
+                .getVisibleText()
+                .then((text) => expect(text).to.equal('1'))
     }
 });
